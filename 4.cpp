184372829d43163fd723f4747ecd571a4ddb8b2a
supriyobaruab <suxpriyo@gmail.com>
2025-01-16 17:43:26 +0600

    completed the 4th one

--- 4.cpp
+++ 4.cpp
@@ -10,13 +10,9 @@ private:
     double semesterGrade;
 
 public:
-    // Default constructor
     Scholar() : scID(0), name(""), semesterGrade(0.0) {}
-
-    // Parameterized constructor
     Scholar(int id, string n, double grade) : scID(id), name(n), semesterGrade(grade) {}
 
-    // Setter method to set scholar information
     void setInfo(int id, string n, double grade)
     {
         scID = id;
@@ -24,7 +20,6 @@ public:
         semesterGrade = grade;
     }
 
-    // Method to display scholar information
     void displayInfo() const
     {
         cout << "Scholar ID: " << scID << endl;
@@ -32,24 +27,20 @@ public:
         cout << "Semester Grade: " << semesterGrade << endl;
     }
 
-    // Method to calculate SGPA with discounts
     double SGPA() const
     {
         double discount = 0.0;
-
         if (semesterGrade >= 9)
         {
-            discount = 0.1; // 10% discount for grades 9 and above
+            discount = 0.1;
         }
         else if (semesterGrade >= 8)
         {
-            discount = 0.05; // 5% discount for grades 8 and above
+            discount = 0.05;
         }
-
         return semesterGrade - (semesterGrade * discount);
     }
 
-    // Method to calculate CGPA after 1 year (2 semesters)
     double CGPA(double secondSemesterGrade) const
     {
         double averageGrade = (semesterGrade + secondSemesterGrade) / 2;
@@ -59,29 +50,22 @@ public:
 
 int main()
 {
-    // Creating objects using the default constructor
     Scholar sc1, sc2;
 
-    // Setting details for sc1
     sc1.setInfo(101, "John Doe", 8.5);
-    // Setting details for sc2
     sc2.setInfo(102, "Jane Smith", 9.2);
 
-    // Displaying information for sc1
     cout << "Scholar 1 Info:" << endl;
     sc1.displayInfo();
     cout << "SGPA: " << sc1.SGPA() << endl;
 
-    // Displaying information for sc2
     cout << "Scholar 2 Info:" << endl;
     sc2.displayInfo();
     cout << "SGPA: " << sc2.SGPA() << endl;
 
-    // Assuming the second semester grades for the CGPA calculation
     double sc1SecondSemesterGrade = 8.7;
     double sc2SecondSemesterGrade = 9.0;
 
-    // Calculating and displaying CGPA after 1 year (2 semesters)
     cout << "Scholar 1 CGPA after 1 year: " << sc1.CGPA(sc1SecondSemesterGrade) << endl;
     cout << "Scholar 2 CGPA after 1 year: " << sc2.CGPA(sc2SecondSemesterGrade) << endl;
 
